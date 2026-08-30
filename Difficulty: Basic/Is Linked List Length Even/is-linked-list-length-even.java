514660f17c1578e455a6a8d480d334a88575a943
Mansi Shourie <mansi.shourie24@gmail.com>
2026-08-30 17:23:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Is Linked List Length Even/is-linked-list-length-even.java
@@ -0,0 +1,26 @@
+/* structure of link list node
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+*/
+class Solution {
+    public boolean isEven(Node head) {
+        // code here
+        Node temp=head;
+        int count=0;
+        while(temp!=null){
+            count++;
+            temp=temp.next;
+        }
+        if(count%2==0){
+            return true;
+        }
+        return false;
+    }
+}
